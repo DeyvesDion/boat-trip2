c4c3f51177c7b59a2410a0ec04e082be2659517f
DeyvesDion <deyvesdion@gmail.com>
2021-06-22 16:26:38 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <div id="nav">
+        <NavBar />
+    </div>
+    <router-view />
 </template>
+<script>
+import NavBar from "@/components/NavBar/NavBar.vue";
+// import Footer from "@/components/Footer/Footer.vue";
+export default {
+    components: {
+        NavBar,
+        // Footer,
+    },
+};
+</script>
 
 <style lang="scss">
+@import url("./views/style/reset.scss");
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Montserrat, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+    padding: 0px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+    a {
+        font-weight: bold;
+        color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
+        &.router-link-exact-active {
+            color: #42b983;
+        }
     }
-  }
 }
 </style>
---
+++ src/components/Banner/Banner.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="banner">
+        <h1 class="banner__title">Location de bateau entre particulier</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Banner",
+    components: {
+        // Banner,
+    },
+};
+</script>
+
+<style src="./Banner.scss" lang="scss" scoped></style>
